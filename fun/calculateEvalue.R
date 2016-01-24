240848b040fe7e9714363c59505a7e145289fc38
jsgeo <schaal@posteo.de>
2016-01-24 20:47:03 +0100

    Update calculateEvalue.R

--- fun/calculateEvalue.R
+++ fun/calculateEvalue.R
@@ -1,3 +1,16 @@
+#' Calculation of the independence-value
+#'
+#' This function calculates the independence-value. 
+#'@author Luise Wraase, Jannik Günther, Henning Reinarz und Johannes Schaal <team1@none.com>,  
+#' \cr
+#' \emph{Maintainer:} Luise Wraase, Jannik Günther, Henning Reinarz und Johannes Schaal \email{team1@none.com}
+#' @references \url{http://www.alpenverein.de/chameleon/public/e73377b2-4b18-f439-1392-c5758cd00d88/Panorama-2-2012-Prominente-Berge_19663.pdf} 
+#' @param altitude The altitude of the peak to be calculated
+#' dom The dominance of the peak to be calculated
+#' prom The prominence of the peak to be calculated
+#' @return The independence-value 
+#' @export calculateEValue
+
 calculateEValue <- function(altitude, dom, prom){
   # Calculates the Prominence Value for a given tuple of coordinates and altitude as derived from DEM 
   #
